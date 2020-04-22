d0266cd64c6d852dbcd5e0a67182818293c32913
LiRiPeng <1014156094@qq.com>
2020-04-22 13:09:28 +0800

    feat: 取消忽略 build 目录

---
+++ build/precache-manifest.572056a7ccc1fc4ad6e30197b5bf5f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68de9d643abf14899e4dd9e81df11662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9850daa8193a876525fa",
+    "url": "/static/css/2.5d732da3.chunk.css"
+  },
+  {
+    "revision": "6c335a8c16600e92e3bb",
+    "url": "/static/css/main.31f31086.chunk.css"
+  },
+  {
+    "revision": "9850daa8193a876525fa",
+    "url": "/static/js/2.1ccad013.chunk.js"
+  },
+  {
+    "revision": "6c335a8c16600e92e3bb",
+    "url": "/static/js/main.a4edbb38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ac419e53d7ed148f3b450db4e16e133",
+    "url": "/static/media/my-avatar.7ac419e5.jpg"
+  }
+]);
